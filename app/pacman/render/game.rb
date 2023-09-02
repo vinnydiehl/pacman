5177fe6ca57d0932fd73a660b50841bb4a8d6df7
Vinny Diehl <vinny.diehl@gmail.com>
2023-09-02 02:36:45 -0400

    Load grid
    Using placeholder solids.

--- app/pacman/render/game.rb
+++ app/pacman/render/game.rb
@@ -1,6 +1,7 @@
 class PacMan
   def render_game
-    render_background
+    render_background ####### for debug;
+    render_map ############## these will be static renders
   end
 
   def render_background
@@ -8,7 +9,37 @@ class PacMan
       primitive_marker: :solid,
       x: 0, y: 0,
       w: @screen_width, h: @screen_height,
-      r: 0, g: 0, b: 0
+      r: 0, g: 0, b: 0,
     }
+  end
+
+  def load_map
+    cell_size = @screen_height / @grid_height
+    grid_px_width = @grid_width * cell_size
+    grid_start_x = (@screen_width / 2) - (grid_px_width / 2)
+
+    colors = {
+      outer_wall: { r: 0, g: 0, b: 255 },
+      inner_wall: { r: 0, g: 0, b: 200 },
+      ghost_house_wall: { r: 0, g: 0, b: 150 },
+    }
+
+    @grid.each_with_index do |column, x|
+      column.each_with_index do |cell, y|
+        cell[:sprite] = {
+          primitive_marker: :solid,
+          x: grid_start_x + (x * cell_size),
+          y: y * cell_size,
+          w: cell_size, h: cell_size,
+          **(colors[cell[:type]] || {}),
+        }
+      end
+    end
+  end
+
+  def render_map
+    @grid.flatten.each do |cell|
+      @primitives << cell[:sprite]
+    end
   end
 end
--- app/pacman/scenes/game.rb
+++ app/pacman/scenes/game.rb
@@ -1,3 +1,55 @@
 class PacMan
+  def game_init
+    cells = {
+      "`" => {
+        type: :empty,
+        walkable: true,
+      },
+      "." => {
+        type: :dot,
+        walkable: true,
+      },
+      "o" => {
+        type: :power_pellet,
+        walkable: true,
+      },
+
+      "+" => {
+        type: :outer_wall,
+        walkable: false,
+      },
+      "X" => {
+        type: :inner_wall,
+        walkable: false,
+      },
+      "H" => {
+        type: :ghost_house_wall,
+        walkable: false,
+      },
+      "-" => {
+        type: :ghost_house_inside,
+        walkable: false,
+      },
+      "=" => {
+        type: :ghost_house_door,
+        walkable: false,
+      },
+    }.freeze
+
+    @grid = @args.gtk.read_file("data/grid.dat").split("\n").map do |line|
+      line.chars.map do |ch|
+        cells[ch].dup
+      end
+    end
+
+    # Rotate so that cell (0, 0) is at the bottom-left
+    @grid = @grid.transpose.map(&:reverse)
+
+    @grid_width = @grid.length
+    @grid_height = @grid.first.length
+
+    load_map
+  end
+
   def game_tick; end
 end
